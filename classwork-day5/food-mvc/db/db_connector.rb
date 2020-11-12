3c4720ab933c0c4462c279c575f8d0602c2f32ed
karen <karenlukitooo@gmail.com>
2020-11-12 17:52:23 +0700

    day5 classwork db

---
+++ classwork-day5/food-mvc/db/db_connector.rb
@@ -0,0 +1,12 @@
+require 'mysql2'
+
+def create_db_client
+    client = Mysql2::Client.new(
+        :host => "127.0.0.1",
+        :username => "root",
+        :password => "1234",
+        :database => ENV["db_name"]
+    )
+    client
+end
+
