8c5be2f480f68d85f1f55f996acf63e68ec8f15b
karen <karenlukitooo@gmail.com>
2020-11-14 23:38:17 +0700

    edit order.rb

--- hw-day4/models/order.rb
+++ hw-day4/models/order.rb
@@ -1,16 +1,9 @@
-require 'mysql_connector.rb'
-require './model/item.rb'
+require './db/db_connector.rb'
+require './models/item.rb'
 
 class Order
     attr_accessor :reference_no, :customer_name, :date, :items
 
-    #create table `orders` (
-    #    `reference_no` varchar(255) not null,
-    #    `customer_name` varchar(255) not null,
-    #    `date` varchar(255) not null,
-    #    primary key (`referece_no`)
-    #);
-    
     def initialize(param)
         @reference_no = param[ :reference_no]
         @customer_name = param[ :customer_name]
@@ -22,6 +15,23 @@ class Order
 
         client = create_db_client
         client.query("insert into orders(reference_no, customer_name, date) values('#{reference_no}', '#{customer_name}'), '#{date}')")
+    end
+
+    def self.edit(id, name, price, category_id)
+        client = create_db_client
+        selected_data = client.query("select * from items where id = #{id}")
+        if selected_data == nil
+            puts("Please insert a valid id")
+        else 
+            client.query("update items i set i.name = '#{name}', i.price = '#{price}' where id = '#{id}'")
+            client.query("update item_categories ic set ic.category_id = '#{category_id}' where item_id = '#{id}'")
+
+            # client.query("UPDATE items i join item_categories ic ON i.id=ic.item_id SET ic.category_id = '#{category_id}', i.name = '#{name}', i.price = '#{price}' where id = '#{id}';
+            
+            
+            #update items set items.name = 'Es Cendol', items.price = '10000' where id = '7';
+            #update item_categories i set i.category_id = '2' where i.item_id = '7'; 
+        end
     end
 
     #TODO:
@@ -49,9 +59,9 @@ class Order
     
     def create_item(params)
         item = Item.new({
-            id: params['id']
-            name: params['name']
-            price: params['price']
+            id: params['id'],
+            name: params['name'],
+            price: params['price'],
             order: self
         })
 
